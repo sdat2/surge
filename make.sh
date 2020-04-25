599d51c773fef3ce20a718824cdd8a5da9ca7122
Simon D.A. Thomas <30407294+sdat2@users.noreply.github.com>
2020-04-25 21:34:30 +0100

    Began writing report etc.

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 # https://tex.stackexchange.com/questions/140845/how-can-i-ignore-latex-error-while-compiling
 
 # options: [main, proposal, presentation]
-filename=presentation
+filename=main # presentation
 
 mv aux/${filename}.lo* aux/${filename}.aux aux/${filename}.ilg .
 mv aux/${filename}.ind aux/${filename}.toc .
@@ -12,14 +12,15 @@ mv aux/${filename}.out aux/${filename}.asc .
 mv aux/${filename}.snm aux/${filename}.fls aux/${filename}.run.xml .
 mv aux/${filename}.nav aux/${filename}.dvi aux/${filename}.fdb_latexmk .
 mv aux/${filename}.vrb  aux/${filename}-blx.bib .
-
 # python tools/mkhistory.py hist.tex # I don't know what this originally did.
+
 
 pdflatex --interaction nonstopmode --shell-escape ${filename}.tex # >/dev/null
 makeindex -c -s myindex.ist ${filename}.idx #2>/dev/null
 bibtex ${filename} #>/dev/null
 makeindex -c -s myindex.ist ${filename}.idx #2>/dev/null
 pdflatex  --interaction nonstopmode  --shell-escape ${filename}.tex #>/dev/null
+
 
 mv ${filename}.lo* *.aux ${filename}.ilg ${filename}.ind ${filename}.toc aux/
 mv ${filename}.bbl ${filename}.blg ${filename}.out *.asc aux/
